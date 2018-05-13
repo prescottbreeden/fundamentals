830a9e4672cdf85b7ceb595926a89f19c4f60bc5
prescott breeden <pb@prescotts-iMac.local>
2018-05-13 01:46:13 -0700

    added custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
+import { SummaryPipe } from './summary.pipe';
 import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,7 +10,30 @@ import { Component } from '@angular/core';
     // template: '<h2>{{ "Title: " + title }}</h2>'
     template: `
     <h2>{{ getTitle() }}</h2>
-    <button class="btn btn-primary" [class.active]="isActive">save</button>
+    <div></div>
+    {{ course.title | uppercase | lowercase }} <br/>
+    {{ course.students | number }} <br/>
+    {{ course.rating | number:'1.2-2' }} <br/>
+    {{ course.price | currency:'EUR':false:'3.2-2' }} <br/>
+    {{ course.releaseDate | date:'shortDate' }} <br/>
+    <div></div>
+    {{ text | summary:10 }}
+    <div></div>
+    <input #emailTag (keyup.enter)="onKeyUp(emailTag.value)" />
+    <div></div>
+    <input [value]="email" (keyup.enter)="email = $event.target.value; onAltKeyUp()" />
+    <div></div>
+    <input [(ngModel)]="email" (keyup.enter)="onAltKeyUp()" />
+    <div></div>
+    <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
+    <div></div>
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Style Binding</button>
+    <div></div>
+    <div (click)="onParentClick()">
+        <div (click)="onDivClick()">
+            <button (click)="onSave($event)">Event Binding</button>
+        </div>
+    </div>
     <ul>
         <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
@@ -27,11 +50,21 @@ import { Component } from '@angular/core';
 export class CoursesComponent {
     title = 'List of Courses';
     courses;
-    isActive = true;
-    // tslint:disable-next-line:max-line-length
-    imageUrl = 'https://images.unsplash.com/photo-1506108198627-b100e11bbc47?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec77747e2fa7b03bb394aa4e78c4434b&auto=format&fit=crop&w=1950&q=80';
+    isActive = false;
+    imageUrl = '';
     colSpan = 2;
+    email = 'me@example.com';
+    // tslint:disable-next-line:max-line-length
+    text = `Mosh (Moshfegh) Hamedani is a passionate and creative software engineer with a strong focus on pragmatism and simplicity. He started programming at the age of seven on a Commodore 64. Later, during his high school years, he started learning C and C++ . In 2002, along with the first release of .NET, he shifted his focus to C#. Since then he has been involved in the design and implementation of numerous software projects, including modern web applications, mobile apps, desktop applications and frameworks. He started teaching on Udemy in 2014 and currently he has over 100,000 students in 182 countries. He's the author of several best-selling courses on Udemy and #1 ranking course on Pluralsight (Become a Full-stack .NET Developer). Mosh has a Master of Science in Network Systems and a Bachelor of Science in Software Engineering. He is also a Microsoft Certified Application Developer, Technology Specialist (Web Applications) and Professional. Outside the software world, Mosh is a photographer, a pianist and a passionate Latin dancer.
+    `;
 
+    course = {
+        title: 'the complete angular course',
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    };
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
@@ -39,5 +72,26 @@ export class CoursesComponent {
 
     getTitle() {
         return this.title;
+    }
+
+    onSave($event) {
+        $event.stopPropagation();
+        console.log('button was clicked', $event);
+    }
+
+    onDivClick() {
+        console.log('div was clicked');
+    }
+
+    onParentClick() {
+        console.log('parent was clicked');
+    }
+
+    onKeyUp(email) {
+        console.log(email);
+    }
+
+    onAltKeyUp() {
+        console.log(this.email);
     }
 }
